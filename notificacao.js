229b10e9f4ce00ea7a7d983b702487e89e03ccff
pyetra <pyetracampos2024@gmail.com>
2026-04-28 22:08:41 -0300

    notificacao.js
    Added function call to start message consumption.

--- notificacao.js
+++ notificacao.js
@@ -2,18 +2,18 @@ const amqp = require('amqplib');
 
 async function ouvirFila() {
     try {
-        // 1. Conecta no RabbitMQ (Docker)
+     
         const connection = await amqp.connect('amqp://localhost');
         const channel = await connection.createChannel();
 
         const fila = 'fila_notificacoes';
 
-        // 2. Garante que a fila existe (importante caso esse serviço suba antes do outro)
+      
         await channel.assertQueue(fila, { durable: false });
 
         console.log(" [*] Aguardando mensagens na fila '%s'. Para sair, pressione CTRL+C", fila);
 
-        // 3. O "Consumidor": Toda vez que chegar uma mensagem, essa função abaixo roda
+        
         channel.consume(fila, (msg) => {
             if (msg !== null) {
                 const conteudo = msg.content.toString();
@@ -23,7 +23,7 @@ async function ouvirFila() {
                 console.log(" [v] Enviando alerta ao usuário... (Sucesso)");
                 console.log(" -----------------------------------------");
 
-                // Avisa ao RabbitMQ que a mensagem foi lida e pode ser descartada
+             
                 channel.ack(msg);
             }
         });
